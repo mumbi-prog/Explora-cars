e45c59fcb4a964f2ccb2a3b972a82997f4cfdd25
donvine mugendi <donvinemugendi@gmail.com>
2023-09-04 23:10:03 +0300

    updated customer model validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < ApplicationRecord
   has_secure_password
   validates_presence_of :password, :email, :age, :mobile_number, :full_name
   validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A(.+)@(.+)\z/, message: "Email invalid"  },length: { minimum: 4, maximum: 254 }
-  validates :mobile_number, numericality: { is: 12 }
+  validates :mobile_number, length: { minimum: 12, maximum: 12 }
   validate :fullname_not_email
   validates :password, length:{minimum: 8}
   validates :age, numericality: { greater_than_or_equal_to: 18, less_than_or_equal_to: 65 }
--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,10 +1,15 @@
+# Rack::Attack.throttle('login/email', limit: 3, period: 60.seconds) do |req|
+#     if req.path == '/login' && req.post?
+#       # req.params['email']
+#       req.ip
+#     end
+#   end
 Rack::Attack.throttle('login/email', limit: 3, period: 60.seconds) do |req|
-    if req.path == '/login' && req.post?
-      # req.params['email']
-      req.ip
-    end
+  if req.path == '/login' && req.post?
+    # req.params['email']
+    req.ip
   end
-  
+end
   Rack::Attack.throttle("requests by ip", limit: 10, period: 1.minutes) do |request|
     request.ip unless request.get?
   end
@@ -18,3 +23,20 @@ Rack::Attack.throttle('login/email', limit: 3, period: 60.seconds) do |req|
   #     req.params['email'].presence
   #   end
   # end
+
+  # Rack::Attack.blocklisted_response = lambda do |env|
+  #   Rack::Response.new(
+  #     status: 429,
+  #     headers: { "Content-Type" => "application/json" },
+  #     body: { errors: ["Too many attempts, try again later."] }.to_json
+  #   )
+  # end
+  Rack::Attack.throttled_response = lambda do |env|
+    status_code = 429
+    headers = {
+      'Content-Type' => 'application/json',
+      'Retry-After' => '60' # You can set the retry time here (in seconds)
+    }
+    body = 'Too many attempts, try again later'
+    [status_code, headers, [body]]
+  end
