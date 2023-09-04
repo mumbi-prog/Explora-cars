54405a4fbf344339e9fd6b6c74beae22ef8a0742
Sylvia Mumbi <mumbi099@gmail.com>
2023-09-04 23:04:28 +0300

    Defined create, update, destroy controller actions

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,16 +6,34 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_car_not_found
         render json: Car.all, status: :ok
     end
 
-  
+    def create
+        car = Car.create!(car_params)
+        render json: car, status: :created
+    end
 
     def show
         car = find_car
         render json: car, status: :ok
     end
 
+    def update
+        car = find_car
+        car.update!(car_params)
+        car.save!
+        render json: car, status: :ok
+    end
+
+    def destroy
+        car = find_car
+        car.destroy!
+        head :no_content
+    end
+
     private
 
-
+    def car_params
+        params.permit(:make, :year, :price_per_day, :image_url, :transmission, :body_type, :category, :fuel_consumption, :no_of_seats, :fuel_type, :is_rented)
+    end
 
     def find_car
         car = Car.find(params[:id])
