f0114c6ee392f0d856f0c065a77a8b593fd7e715
Sylvia Mumbi <mumbi099@gmail.com>
2023-09-04 21:39:08 +0300

    Added ano hybridl type

--- app/models/car.rb
+++ app/models/car.rb
@@ -9,7 +9,7 @@ class Car < ApplicationRecord
   validates :category, presence: true, inclusion: { in: ['Luxury', 'Sport', 'Compact', 'Economy' 'Electric', 'Hybrid', 'Off-road', 'Vans and minivans'] }
   validates :fuel_consumption, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :no_of_seats, presence: true, numericality: { only_integer: true, greater_than: 1 }
-  validates :fuel_type, presence: true, inclusion: { in: ['gasoline', 'diesel', 'electric', 'other'] }
+  validates :fuel_type, presence: true, inclusion: { in: ['gasoline', 'diesel', 'electric', 'hybrid'] }
 
   def available?
     !is_rented
