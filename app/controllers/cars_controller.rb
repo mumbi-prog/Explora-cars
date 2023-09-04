f472ff2a71c367099188860cf7060aa4983a43d5
Sylvia Mumbi <mumbi099@gmail.com>
2023-09-04 22:47:30 +0300

    Defined private methods for car controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,18 @@
 class CarsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+rescue_from ActiveRecord::RecordNotFound, with: :render_car_not_found
+
+    private
+
+    def find_car
+        car = Car.find(params[:id])
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
+    def render_car_not_found
+        render json: {error: 'Car not found, try again'}, status: :not_found
+    end
 end
