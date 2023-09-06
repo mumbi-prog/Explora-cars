d5fdbacad90b89bd266e90c06a25312270f98846
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-09-06 23:45:35 +0300

    specifying the routes in use

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,5 @@
 class Location < ApplicationRecord
     has_many :cars
-
     validates :name, presence: true
     validates :phone_number, numericality: { is: 12 }
     validates :address, presence: true
