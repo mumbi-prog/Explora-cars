12c26824412530eaa8999ef5a0183860ad8aa8cc
Maureen <morynmoh@gmail.com>
2023-09-07 07:34:02 +0300

    reviews controller added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
+# app/controllers/reviews_controller.rb
 class ReviewsController < ApplicationController
+  before_action :set_car
+
+  def index
+    @reviews = @car.reviews
+    render json: @reviews
+  end
+
+  def create
+    @review = @car.reviews.build(review_params)
+    if @review.save
+      render json: @review, status: :created
+    else
+      render json: @review.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:car_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:title, :body, :rating)
+  end
 end
