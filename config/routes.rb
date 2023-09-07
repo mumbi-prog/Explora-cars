c7b41223c5295e1094460f2197ab31daa0f67b0a
donvine mugendi <donvinemugendi@gmail.com>
2023-09-07 18:54:18 +0300

    updated database.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   # route to test your configuration
   # get '/hello', to: 'application#hello_world'
+  resources :locations, only: [:index, :show]
+  resources :cars, only: [:index, :create, :show, :update, :destroy]
+  resources :customers, only:[:index, :update, :destroy]
+  post "/signup", to: "customers#create"
+  get "/me", to:"customers#show"
+  post "/login", to:"sessions#create"
+  delete "logout", to:"sessions#destroy"
+  resources :bookings
+  get "/car_bookings/:id",to:"bookings#car_dates"
+  get "/customer_bookings/:id",to:"bookings#customer_bookings"
+  # route to test your configuration
+  # get '/hello', to: 'application#hello_world'
+  resources :reviews, only: [:index, :create]
 end
