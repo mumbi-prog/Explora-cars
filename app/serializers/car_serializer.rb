78988588982ef865b4356142996f23fe9febfe81
Sylvia Mumbi <mumbi099@gmail.com>
2023-09-08 17:15:23 +0300

    Add has_many association for bookings in CarSerializer

--- app/serializers/car_serializer.rb
+++ app/serializers/car_serializer.rb
@@ -1,3 +1,4 @@
 class CarSerializer < ActiveModel::Serializer
   attributes :id, :make, :year, :price_per_day, :image_url, :transmission, :body_type, :category, :fuel_consumption, :no_of_seats, :fuel_type, :is_rented
+  has_many :bookings
 end
